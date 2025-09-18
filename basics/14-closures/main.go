46c3debb60f61674892175aec8dad4d1f6a8612e
Nimesh <nimesh.shrestha@ebpearls.com>
2025-09-18 17:10:32 +0545

    15 recursion complete

--- basics/14-closures/main.go
+++ basics/14-closures/main.go
@@ -1,7 +1,20 @@
-
-
 package main
 
-func main() {
+import "fmt"
+
+
+func closureInt()func()int  {
+	i:=0
+	return func () int {
+		i++
+		return i
+	}
 	
+}
+func main() {
+	nextInt:=closureInt()
+
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
 }
