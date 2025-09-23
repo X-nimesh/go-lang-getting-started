64d26abc7a9ada7b091aa1814875aa28ea23e065
Nimesh <x.nimesh7@gmail.com>
2025-09-23 10:01:37 +0545

    18-strings and runes 2 c

--- basics/18-strings-and-runes/main.go
+++ basics/18-strings-and-runes/main.go
@@ -13,4 +13,9 @@ const n="नमस्कार";
 			fmt.Printf("%x",n[i])
 	}
 	fmt.Println("Rune count:",utf8.RuneCountInString(n))
+	
+	for idx, runeValue:= range n{
+		fmt.Printf("%#U starts at %d\n",runeValue,idx)
+	}
+
 }
